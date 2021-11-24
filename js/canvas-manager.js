93c6fbaacfd16354e9b3b9582eeac30927bbc047
cdaly333 <dalyco884@gmail.com>
2021-11-23 21:16:23 -0500

    add log

--- js/canvas-manager.js
+++ js/canvas-manager.js
@@ -95,6 +95,7 @@ export class CanvasManager {
 
 		// Create an HD canvas
 		let ratio = window.devicePixelRatio || 1;
+		console.log(ratio);
 		let width = document.body.clientWidth;
 		let height = Math.max(document.body.clientHeight, 7500);
 		this.canvas = document.createElement("canvas");
